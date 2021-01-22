bf3dabf2856be464001634ceaceab852a3f5591e
Nathan <nathanjones1@icloud.com>
2021-01-22 21:26:54 +0000

    Set up initil component files

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+  render(){
+    let name = "nathan";
+    
+    return (
+      <View style={styles.container}>
+        <Text>Hello {name}</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+  
 }
 
 const styles = StyleSheet.create({
